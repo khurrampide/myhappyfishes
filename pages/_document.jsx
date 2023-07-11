f43935f58944fb82d56b05fd993cb3ca236c04dc
Khurram Iqbal <67738524+khurrampide@users.noreply.github.com>
2023-07-11 15:56:59 +0500

    Update _document.jsx
    Google Analytics

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -8,6 +8,15 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         ></link>
+        <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LGZH8RE5SG"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-LGZH8RE5SG');
+</script>
       </Head>
       <body>
         <Main />
